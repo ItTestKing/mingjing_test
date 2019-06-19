408d565e4c4dae9f332c9a6e9bbeb74ba2542166
likangkang08 <1712340552@qq.com>
2019-06-19 15:08:11 +0800

    自动化测试携程

--- demo/test_case.py
+++ demo/test_case.py
@@ -1,70 +1,18 @@
-from pyse import Pyse, TestCase, TestRunner
-from parameterized import parameterized
+# coding=utf-8
+import pyse
+from time import sleep
 
-
-class BaiduTest(TestCase):
-    """Baidu serach test case"""
-
-    @classmethod
-    def setUpClass(cls):
-        """ Setting browser driver, Using chrome by default."""
-        cls.driver = Pyse("chrome")
-        cls.timeout = 15  # You can customize timeout time
-
-    """
-    A simple test
-    """
-    def test_case(self):
-        """ baidu search key : pyse """
-        self.open("https://www.baidu.com/")
-        self.max_window()
-        self.move_to_element("link_text=>设置")
-        self.click("link_text=>搜索设置")
-        self.select("#nr", '20')
-        self.click("class=>prefpanelgo")
-        self.sleep(2)
-        self.assertAlert("已经记录下您的使用偏好")
-        self.accept_alert()
-
-
-    """
-    
-    used parameterized test
-    """
-    @parameterized.expand([
-        (1, 'pyse'),
-        (2, 'selenium'),
-        (3, 'unittest'),
-    ])
-    def test_baidu(self,name,search_key):
-        ''' baidu search key : pyse '''
-        self.open("https://www.baidu.com")
-        self.max_window()
-        self.clear("id=>kw")
-        self.type("id=>kw", search_key)
-        self.sleep(5)
-        self.click("css=>#su")
-        self.assertTitle(search_key)
-
-    def test_mingjing(self):
-        self.open("https://192.168.0.172")
-        self.max_window()
-        self.sleep(10)
-
-
-
+def test_baidu():
+    ''' baidu search key : pyse '''
+    driver = pyse.Pyse("chrome")
+    driver.open("https://www.baidu.com")
+    driver.type("#kw","pyse")
+    driver.click("#su")
+    sleep(1)
+    title = driver.get_title()
+    assert title=="pyse_百度搜索"
+    driver.quit()
 
 if __name__ == '__main__':
-    runner = TestRunner('./', '百度测试用例', '测试环境：Firefox')
-    runner.debug()
-
-'''
-说明：
-'./' ： 指定测试目录。
-'百度测试用例' ： 指定测试项目标题。
-'测试环境：Chrome' ： 指定测试环境描述。
-
-debug() # debug模式不生成测试报告
-run()   # run模式生成测试报告
-'''
-
+    test_pro = pyse.TestRunner()
+    test_pro.run()
